0ca15940ca800de3475bfba13fc87713cbaeff14
Maksym Karlash <xxmaxdrivezz@gmail.com>
2020-06-19 00:46:52 +0300

    test

---
+++ Tanks/Models/Units/DropUnit.cs
@@ -0,0 +1,12 @@
+﻿using Tanks.Models.Units.Interfaces;
+using Tanks.Models.Units.UnitModels;
+
+namespace Tanks.Models.Units
+{
+    [UnitState(UnitState.Drop)]
+    public class DropUnit : AbstractUnit,IDrop
+    {
+        public DropUnit(Coordinates coordinates) : base(coordinates){}
+        public DropUnit(int x, int y) : this(new Coordinates(x, y)) { }
+    }
+}
